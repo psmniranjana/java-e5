33f38e0879580b674b392fa6d49a8af02d0c84f8
Maleesha Niranjana <117069472+psmniranjana@users.noreply.github.com>
2023-04-06 14:05:23 +0530

    Add files via upload

---
+++ JavaApplication35.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication35;
+import java.util.Scanner;
+
+public class JavaApplication35 {
+
+    public static void main(String[] args) {
+              Scanner read = new Scanner(System.in);
+       int num1 = read.nextInt();
+       int num2 = read.nextInt();
+
+       //your code goes here
+       System.out.println(Math.pow(num1,num2));
+   }
+    }
+    
+
